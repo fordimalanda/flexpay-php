b7faafb272cb6f4a494568895789ae67433a2e32
Fordi Malanda <fordimalanda7@gmail.com>
2026-05-15 09:55:03 +0200

    refactor(response): align snake_case API data with camelCase properties in responses

--- src/Response/CardResponse.php
+++ src/Response/CardResponse.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace Devscast\Flexpay\Response;
 
 use Devscast\Flexpay\Data\Status;
+use Symfony\Component\Serializer\Attribute\SerializedName;
 
 /**
  * Class CardResponse.
+ * * Représente la réponse suite à une demande de paiement par carte.
+ * Elle inclut l'URL de redirection vers la passerelle de paiement.
  *
  * @author bernard-ng <bernard@devscast.tech>
  */
@@ -16,7 +19,10 @@ final class CardResponse extends FlexpayResponse
     public function __construct(
         public Status $code,
         public string $message = '',
+        
+        #[SerializedName('orderNumber')]
         public ?string $orderNumber = null,
+        
         public ?string $url = null
     ) {
     }
--- src/Response/PayoutResponse.php
+++ src/Response/PayoutResponse.php
@@ -5,12 +5,19 @@ declare(strict_types=1);
 namespace Devscast\Flexpay\Response;
 
 use Devscast\Flexpay\Data\Status;
+use Symfony\Component\Serializer\Attribute\SerializedName;
 
+/**
+ * Class PayoutResponse.
+ * * Représente la réponse suite à une demande de Payout (versement).
+ */
 final class PayoutResponse extends FlexpayResponse
 {
     public function __construct(
         public Status $code,
         public string $message = '',
+        
+        #[SerializedName('orderNumber')]
         public ?string $orderNumber = null,
     ) {
     }
